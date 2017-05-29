357b33c677c5cae252da7d145a19613ec04ac74a
Touhid Mia <hmtm.cse@gmail.com>
2017-05-30 04:43:59 +0600

    Scaffold kit

--- dot_net_core/scaffold/Scaffold/DynamicUIGenerator/ModelReader.cs
+++ dot_net_core/scaffold/Scaffold/DynamicUIGenerator/ModelReader.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace Scaffold.DynamicUIGenerator
